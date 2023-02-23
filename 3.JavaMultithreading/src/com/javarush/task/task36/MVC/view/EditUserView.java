6b9160ec6cd6d9ad0528a5eaeec66c2bc4c365cc
Dmitry <eplar@yandex.ru>
2023-02-23 18:30:19 +0300

    Initial commit

---
+++ 3.JavaMultithreading/src/com/javarush/task/task36/MVC/view/EditUserView.java
@@ -0,0 +1,29 @@
+package com.javarush.task.task36.MVC.view;
+
+import com.javarush.task.task36.MVC.controller.Controller;
+import com.javarush.task.task36.MVC.model.ModelData;
+
+public class EditUserView implements View {
+
+    private Controller controller;
+
+    @Override
+    public void refresh(ModelData modelData) {
+        System.out.println("User to be edited:");
+        System.out.println("\t" + modelData.getActiveUser());
+        System.out.println("===================================================");
+    }
+
+    @Override
+    public void setController(Controller controller) {
+        this.controller = controller;
+    }
+
+    public void fireEventUserDeleted(long id) {
+        controller.onUserDelete(id);
+    }
+
+    public void fireEventUserChanged(String name, long id, int level) {
+        controller.onUserChange(name, id, level);
+    }
+}
